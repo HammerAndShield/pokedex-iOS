96940906c23271ebea3cef9b76c4bb61b6553734
austintownsend <austintownsend1234@gmail.com>
2025-04-17 20:13:09 -0700

    Initial Commit

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by austin townsend on 4/17/25.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
